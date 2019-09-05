67f6f1ad07fd95fed41f21655d7619b31907f058
aishamu <aishamutip@hotmail.com>
2019-09-05 15:23:19 +0300

    edit

--- config/auth.php
+++ config/auth.php
@@ -67,8 +67,8 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-             'model' => App\Models\User::class,
-
+            'model' => App\User::class,
+            
         ],
 
         // 'users' => [
